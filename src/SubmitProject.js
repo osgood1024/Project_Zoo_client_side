77adfbdb4e8d310630ff182a0910b2bb30bd649e
osgood gunawan <ogunawan@ucsd.edu>
2020-05-18 01:47:41 -0400

    working on add new project

--- src/SubmitProject.js
+++ src/SubmitProject.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Button, Form, Segment, TextArea } from 'semantic-ui-react'
 import styled from 'styled-components'
 
@@ -19,38 +19,79 @@ p{
     padding: 10px;
 }
 
+
 `
 
- export const SubmitProject = () => (
+export class  SubmitProject extends Component {
 
-<Styles>
+    state={
+        projectName : "" ,
+        projectLink: "" ,
+        projectPic: "" ,
+        projectDescription: ""
+    }
 
-  <Segment inverted padded={"very"} className={'form_layout'}>
+    handleChange=(e)=>{
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
 
-    <Form inverted>
 
-        <Form.Group widths='equal'>
-            <Form.Input fluid label='Project Name:' placeholder='what is your project topic?' />
-        </Form.Group>
-            <Form.Input fluid label='# Tag :' placeholder='#todolist #react #web_dev....' />
-        <Form.Group widths="grouped">
-            <Form.Input fluid label='Website live link or Github Repo:' placeholder='your project link/ Github repo..' />
-        </Form.Group>
-        <Form.Group widths="grouped">
-            <Form.Input fluid label='Picture for project: ' placeholder='copy image address' />
-        </Form.Group>
+    handleSubmit=(e) =>{
+        e.preventDefault()
+        fetch('http://localhost:3000/projects',{
+            method: "POST",
+            headers:{
+              "content-type" : "application/json",
+              accept : "application/json"
+          },
+          body: JSON.stringify({
+            projectName : this.state.projectName,
+            projectLink: this.state.projectLink,
+            projectPic: this.state.projectPic ,
+            projectDescription: this.state.projectDescription
+             
+          })
+        })
+        .then(resp => resp.json())
+        .then(newProject => console.log(newProject))
+    }
+    
+        
+    
 
-        <p>Description:</p>
-        <Form.Group widths ="grouped">
-        <TextArea  placeholder='Tell us more' />
-        </Form.Group>
 
-     
-      <Button type='submit' className={"button"} >Submit</Button>
+render(){
+    console.log(this.state)
+    return(
+        <Styles>
+        <Segment inverted padded={"very"} className={'form_layout'}>
+            <Form inverted>
 
-    </Form>
+                <Form.Group widths='equal'>
+                    <Form.Input fluid label='Project Name:' placeholder='what is your project topic?' name='projectName' value={this.state.projectName} onChange={this.handleChange} />
+                </Form.Group>
+                    {/* <Form.Input fluid label='# Tag :' placeholder='#todolist #react #web_dev....' /> */}
+                <Form.Group widths="grouped">
+                    <Form.Input fluid label='Website live link or Github Repo:' placeholder='your project link/ Github repo..' name='projectLink' value={this.state.projectLink} onChange={this.handleChange}/>
+                </Form.Group>
+                <Form.Group widths="grouped">
+                    <Form.Input fluid label='Picture for project: ' placeholder='copy image address' name='projectPic' value={this.state.projectPic} onChange={this.handleChange}/>
+                </Form.Group>
 
-  </Segment>
+                <p>Description:</p>
+                <Form.Group widths ="grouped">
+                <TextArea  placeholder='Tell us more' name='projectDescription' value={this.state.projectDescription} onChange={this.handleChange}/>
+                </Form.Group>
 
-</Styles>
-)
+            
+            <Button type='submit' className={"button"} onSubmit={this.handleSubmit} >Submit</Button>
+
+            </Form>
+        </Segment>
+        </Styles>
+
+        )
+    }
+}
