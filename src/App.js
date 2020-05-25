595ab2f9ad6084be952c20585774d8c3034b75e5
osgood gunawan <ogunawan@ucsd.edu>
2020-05-25 15:30:54 -0400

    everythings working now

--- src/App.js
+++ src/App.js
@@ -16,238 +16,19 @@ import { NavigationBar } from './components/NavigationBar';
  class App extends Component{
 
   state={
-    projects:[],
-    comments:[],
-    users:[],
-    favorites:[],
-    searchTerm:'',
-    category: 'all',
+    searchTerm:''
   }
 
 
-componentDidMount(){
-
-  fetch('http://localhost:3000/projects')
-  .then(resp => resp.json())
-  .then(data => this.setState({
-      projects: data
+  handleSearch=(e) =>{
+    this.setState({
+        searchTerm: e.target.value
     })
-  )
-  fetch('http://localhost:3000/comments')
-  .then(resp => resp.json())
-  .then(commented => this.setState({
-      comments: commented
-  }))
-
-  fetch('http://localhost:3000/users')
-  .then(resp=>resp.json())
-  .then(user => this.setState({
-      users: user
-  })
-  )
-  console.log("Go through here")
-  fetch('http://localhost:3000/favorites')
-  .then(resp => resp.json())
-  .then(fav => this.setState({
-      favorites : fav
-  }))
-
-
-}
-
-
-handleSearch=(e) =>{
-  this.setState({
-      searchTerm: e.target.value
-  })
-}
-
-
-filterProject = (category) => {
-
-  this.setState({
-    category: category
-  })
-  // const filterProject= this.state.projects.filter(project => project.category.toLowerCase() === category)
-    
-  // console.log(filterProject)
-
-  // return(
-   
-
-  //   category!=="All" ?  filterProject : this.state.projects
-
-  // )
-
-}
-
-
-handleLike = (id,newLike) => {
-
-  fetch(` http://localhost:3000/projects/${id}`, {
-    method: "PATCH",
-    headers:{
-      "content-type" : "application/json",
-      accept : "application/json"
-  },
-  body: JSON.stringify({
-      like: newLike 
-  })
-})
-  .then(resp => resp.json())
-  .then(updatedProject =>  this.addLike(updatedProject)) 
-
-}
-
-
-addLike = (newProject)=>{
-    
-
-  let current_project = this.state.projects.reduce((acc,currVal) => { 
-    if(currVal.id === newProject.id) {
-       return acc.concat([newProject])
-    } else{
-      return acc.concat([currVal])
-    }
-  }, [])
-  
-let favorite=this.state.favorites.map(fav => {
-  if(fav.project.id === newProject.id ){
-    return {...fav, project: newProject }
-  }else{
-    return fav
   }
-} )
 
-  this.setState({
-    projects: current_project,
-    favorites: favorite
-  })
-}
-
-
-
-handleFavorite =(ProjectId)=>{
-
-  fetch(`http://localhost:3000/favorites`, {
-    method: "POST",
-    headers:{
-      "content-type" : "application/json",
-      accept : "application/json"
-  },
-  body: JSON.stringify({
-      user_id: 1,
-      project_id: ProjectId
-  })
-})
-.then(resp => resp.json())
-.then(newFav => {
-    if(newFav.status===490){
-      alert(newFav.errors)
-    }
-    else{
-      console.log("Setting favorites here")
-      this.setState({
-        favorites: [...this.state.favorites,newFav]
-        })
-    }
-  }
-)
-}
-
-
-handleFavoriteDel=(project_id,favorite_id)=>{
-  console.log('handleFavoriteDel' , favorite_id, 'projectId', project_id)
-    fetch(`http://localhost:3000/favorites/${favorite_id}`, {
-        method: "Delete",
-        headers:{
-          "content-type" : "application/json",
-          accept : "application/json"
-        }
-    })
-    .then(resp => resp.json())
-    .then(()=> this.deleteFav(project_id))
-}
-
-
-
-deleteFav=(id)=>{
-  let favproject =this.state.projects.filter(fav => fav.id !== id)
-  this.setState({
-    projects: favproject
-  })
-
-}
-
-
-
-handleComment=(user_id,ProjectId,newcontent)=>{
-
-    fetch('http://localhost:3000/comments', {
-      method: "POST",
-        headers:{
-          "content-type" : "application/json",
-          accept : "application/json"
-        },
-        body: JSON.stringify({
-          user_id: user_id,
-          project_id: ProjectId,
-          content: newcontent
-          })
-        })
-        .then(resp => resp.json())
-        .then(newComment => {
-          if(newComment.status === 490){
-            alert(newComment.errors)
-          }else{
-            this.setState({
-              comments: [...this.state.comments,newComment]
-            })
-
-          }
-        }
-    )
-    
-}
-
-
-
-handleDelComment=(cid) =>{
-    fetch(`http://localhost:3000/comments/${cid}`, {
-    method: "Delete",
-    headers:{
-    "content-type" : "application/json",
-    accept : "application/json"
-    }
-    })
-    .then(resp => resp.json())
-    .then(()=> this.deleteComment(cid))
-
-}
-
-
-deleteComment=(cid)=>{
-    let comment =this.state.comments.filter(comment => comment.id !== cid)
-      this.setState({
-        comments: comment
-      })
-}
-
-
-
-addProject=(newProject)=>{
-  this.setState({
-    projects:[...this.state.projects,newProject]
-  }, () => this.props.history.push('/'))
-    
-  
-}
 
 
  render(){  
-
-  // console.log(this.state.projects)
-  console.log('app favorites' , this.state.favorites)
 
     return(
       
@@ -256,40 +37,11 @@ addProject=(newProject)=>{
         <Layout>
         
             <Switch >
-              
-              <Route exact path="/" render={props => <Home {...props} 
-               search={this.state.searchTerm} 
-               favorites={this.state.favorites}
-               filterProject={this.filterProject}
-               category={this.state.category}
-               users={this.state.users}
-               comments={this.state.comments}
-               projects={this.state.projects}
-               handleFavorite={this.handleFavorite} 
-               handleFavoriteDel={this.handleFavoriteDel}
-               handleLike={this.handleLike}
-               handleComment={this.handleComment}
-               handleDelComment={this.handleDelComment}
-               
-               />}/>
-              <Route path="/favorite" render={props => <Favorite {...props} 
-              search={this.state.searchTerm}
-              category={this.state.category}
-              filterProject={this.filterProject}
-              favorites={this.state.favorites}
-              users={this.state.users}
-              comments={this.state.comments}
-              projects={this.state.projects}
-              handleFavorite={this.handleFavorite} 
-              handleFavoriteDel={this.handleFavoriteDel}
-              handleLike={this.handleLike}
-              handleComment={this.handleComment}
-              handleDelComment={this.handleDelComment}
-            
-              
-              />} />
-              <Route path="/submitproject" render={ props => <SubmitProject {...props} projects={this.state.projects} 
-              newProject={this.addProject} />}/>
+              <Route exact path="/" render={props => <Home {...props} search={this.state.searchTerm} />} 
+              />
+              <Route path="/favorite" render={props => <Favorite {...props}search={this.state.searchTerm}/>} 
+              />
+              <Route path="/submitproject" component={SubmitProject}/>
               <Route path="/login" component={LogIn}/>
             </Switch>
 
--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,204 @@ import ProjectList from './components/ProjectList'
 export class Home extends React.Component{
 
 
-    render(){
-        const {category} = this.props;
+    state={
+        projects:[],
+        comments:[],
+        users:[],
+        favorites:[],
+        category: 'all'
+    }
 
-        const searchFilter = this.props.projects.filter(p => p.name.toLowerCase().includes(this.props.search.toLowerCase()))
+ componentDidMount(){
+     
+    fetch('http://localhost:3000/projects')
+        .then(resp => resp.json())
+        .then(data => this.setState({
+            projects: data
+        })
+        )
+
+    fetch('http://localhost:3000/comments')
+        .then(resp => resp.json())
+        .then(commented => this.setState({
+            comments: commented
+        })
+        )
+
+    fetch('http://localhost:3000/users')
+        .then(resp=>resp.json())
+        .then(user => this.setState({
+            users: user
+        })
+        )
+
+    fetch('http://localhost:3000/favorites')
+        .then(resp => resp.json())
+        .then(fav => this.setState({
+            favorites : fav
+        }))
+}
+
+
+
+filterProject = (category) => {
+    this.setState({
+      category: category
+    })
+  }
+  
+  
+  handleLike = (id,newLike) => {
+  
+    fetch(` http://localhost:3000/projects/${id}`, {
+      method: "PATCH",
+      headers:{
+        "content-type" : "application/json",
+        accept : "application/json"
+    },
+    body: JSON.stringify({
+        like: newLike 
+    })
+  })
+    .then(resp => resp.json())
+    .then(updatedProject =>  this.addLike(updatedProject)) 
+  
+  }
+  
+  
+  addLike = (newProject)=>{
+      
+    let current_project = this.state.projects.reduce((acc,currVal) => { 
+      if(currVal.id === newProject.id) {
+         return acc.concat([newProject])
+      } else{
+        return acc.concat([currVal])
+      }
+    }, [])
+    
+  let favorite=this.state.favorites.map(fav => {
+    if(fav.project.id === newProject.id ){
+      return {...fav, project: newProject }
+    }else{
+      return fav
+    }
+  } )
+  
+    this.setState({
+      projects: current_project,
+      favorites: favorite
+    })
+  }
+  
+  
+  
+  handleFavorite =(ProjectId)=>{
+  
+    fetch(`http://localhost:3000/favorites`, {
+      method: "POST",
+      headers:{
+        "content-type" : "application/json",
+        accept : "application/json"
+    },
+    body: JSON.stringify({
+        user_id: 1,
+        project_id: ProjectId
+    })
+  })
+  .then(resp => resp.json())
+  .then(newFav => {
+      if(newFav.status===490){
+        alert(newFav.errors)
+      }
+      else{
+        this.setState({
+          favorites: [...this.state.favorites,newFav]
+          })
+      }
+    }
+  )
+  }
+  
+  handleFavoriteDel=(project_id,favorite_id)=>{
+    console.log('handleFavoriteDel' , favorite_id, 'projectId', project_id)
+      fetch(`http://localhost:3000/favorites/${favorite_id}`, {
+          method: "Delete",
+          headers:{
+            "content-type" : "application/json",
+            accept : "application/json"
+          }
+      })
+      .then(resp => resp.json())
+      .then(()=> this.deleteFav(project_id))
+  }
+  
+  deleteFav=(id)=>{
+    let favproject =this.state.projects.filter(fav => fav.id !== id)
+    this.setState({
+      projects: favproject
+    })
+  }
+  
+  
+  
+  handleComment=(user_id,ProjectId,newcontent)=>{
+  
+      fetch('http://localhost:3000/comments', {
+        method: "POST",
+          headers:{
+            "content-type" : "application/json",
+            accept : "application/json"
+          },
+          body: JSON.stringify({
+            user_id: user_id,
+            project_id: ProjectId,
+            content: newcontent
+            })
+          })
+          .then(resp => resp.json())
+          .then(newComment => {
+            if(newComment.status === 490){
+              alert(newComment.errors)
+            }else{
+              this.setState({
+                comments: [...this.state.comments,newComment]
+              })
+  
+            }
+          }
+      )
+      
+  }
+  
+  
+  
+  handleDelComment=(cid) =>{
+      fetch(`http://localhost:3000/comments/${cid}`, {
+      method: "Delete",
+      headers:{
+      "content-type" : "application/json",
+      accept : "application/json"
+      }
+      })
+      .then(resp => resp.json())
+      .then(()=> this.deleteComment(cid))
+  
+  }
+  
+  
+  deleteComment=(cid)=>{
+      let comment =this.state.comments.filter(comment => comment.id !== cid)
+        this.setState({
+          comments: comment
+        })
+  }
+  
+ 
+
+    render(){
+        const {category} = this.state;
+
+        const searchFilter = this.state.projects.filter(p => p.name.toLowerCase().includes(this.props.search.toLowerCase()))
         
         const search = category  === 'all' ? searchFilter : searchFilter.filter(project => project.category.toLowerCase() === category)
         
@@ -18,15 +212,15 @@ export class Home extends React.Component{
             <div>
                <ProjectList 
                 projects={search} 
-                filterProject={this.props.filterProject}
-                favorites={this.props.favorites}
-                comments={this.props.comments}
-                users={this.props.users}
-                handleLike={this.props.handleLike}
-                handleFavorite={this.props.handleFavorite}
-                handleFavoriteDel={this.props.handleFavoriteDel}
-                handleComment={this.props.handleComment}
-                handleDelComment={this.props.handleDelComment}
+                filterProject={this.filterProject}
+                favorites={this.state.favorites}
+                comments={this.state.comments}
+                users={this.state.users}
+                handleLike={this.handleLike}
+                handleFavorite={this.handleFavorite}
+                handleFavoriteDel={this.handleFavoriteDel}
+                handleComment={this.handleComment}
+                handleDelComment={this.handleDelComment}
                 />
             </div>
        
